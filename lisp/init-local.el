c1bea1c86b9cb272e619c6b618fc7795a4a3d128
ltnwgl <ltnwgl@gmail.com>
2014-08-24 18:09:59 +0800

    add personal working setup for rails

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -16,4 +16,30 @@
 
 (setq save-interprogram-paste-before-kill nil)
 
+;;; personal working setup for rails
+(defun remove-rails-tmp ()
+  "remove the dir tmp of current rails project if exist"
+  (shell-command (concat "rm -rf " (rinari-root) "/tmp")))
+
+(defun rinari-web-server-jp (&optional edit-cmd-args)
+  "Start JP site with EDIT-CMD-ARGS."
+  (interactive "P")
+  (remove-rails-tmp)
+  (rvm-activate-corresponding-ruby )
+  (setenv "SITE_REGION" "JP")
+  (if (string-match "site-tf" (rinari-root))  (setenv "RAILS_ENV" "staging")  (setenv "RAILS_ENV" "development") )
+  (rinari-web-server edit-cmd-args))
+
+(defun rinari-web-server-us (&optional edit-cmd-args)
+  "Start US site with EDIT-CMD-ARGS."
+  (interactive "P")
+  (remove-rails-tmp)
+  (rvm-activate-corresponding-ruby )
+  (setenv "SITE_REGION" "US")
+  (setenv "RAILS_ENV" "development")
+  (rinari-web-server edit-cmd-args))
+
+(global-set-key (kbd "C-c ; j") 'rinari-web-server-jp)
+(global-set-key (kbd "C-c ; u") 'rinari-web-server-us)
+
 (provide 'init-local)
