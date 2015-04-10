fdea6a3f989700f1ae91ee7746868f0eccf414ef
Gengliang Wang <gewang@linkedin.com>
2015-04-10 13:05:48 +0800

    don't user ~ for home dir..

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -1,12 +1,16 @@
 ;;; Basic ruby setup
 (require-package 'go-mode)
 
-(setenv "PATH" "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:~/chitu/go/bin:/usr/local/opt/go/libexec/bin")
-(setenv "GOPATH" "~/chitu/go")
-(add-to-list 'load-path "~/chitu/go/src/github.com/dougm/goflymake")
+(setenv "GOPATH" "/Users/gewang/chitu/go")
+(setenv "PATH" "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/Users/gewang/chitu/go/bin:/usr/local/opt/go/libexec/bin")
+(add-to-list 'load-path "/Users/gewang/chitu/go/src/github.com/dougm/goflymake")
+(setq exec-path (cons "/usr/local/opt/go/libexec" exec-path))
+(add-to-list 'exec-path "/Users/gewang/chitu/go")
 (require 'go-flymake)
 (require 'go-flycheck)
 (require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
 (defun my-go-mode-hook ()
   ; Call Gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
@@ -16,8 +20,7 @@
            "go build -v && go test -v && go vet"))
   ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump))
-(setq exec-path (cons "/usr/local/opt/go/libexec" exec-path))
-(add-to-list 'exec-path "/Users/gewang/chitu/go")
+
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (add-hook 'before-save-hook 'gofmt-before-save)
